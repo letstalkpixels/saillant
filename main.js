492d48f0e0367e64eeaee9686719818c427f0f15
Talkpixels <74625740+Talkpixels@users.noreply.github.com>
2022-10-25 12:07:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(".splide").each((index) => {
     focus: 0, // 0 = left and 'center' = center
     slideFocus: true,
     type: "slide", // 'loop' or 'slide'
-    gap: "1rem", // space between slides
+    gap: "0.5rem", // space between slides
     arrows: false, // 'slider' or false
     speed: 600, // transition speed in miliseconds
     dragAngleThreshold: 30, // default is 30
